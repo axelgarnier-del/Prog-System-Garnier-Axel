10f1e5e91cdab45672d213bb8ba8b6bf359130e8
AXEL GARNIER <axel.garnier@iut.local>
2026-09-16 12:01:06 +0200

    Commencement de la class utils

--- TP2_VFS/Utils.java
+++ TP2_VFS/Utils.java
@@ -3,21 +3,33 @@ public class Utils {
     public static int writeInt(byte[] memory, int offset, int value) {
         // TODO: Écrire les 4 octets de 'value' dans 'memory'
         // à partir de 'offset', en big-endian.
+		memory[offset]     = (byte) (value >>> 24);
+        memory[offset + 1] = (byte) (value >>> 16);
+        memory[offset + 2] = (byte) (value >>> 8);
+        memory[offset + 3] = (byte) value;
+		
         return 4;
     }
 
     public static int readInt(byte[] memory, int offset) {
         // TODO: Reconstituer le int sur 4 octets.
-        return 0;
+		return ((memory[offset] & 0xFF) << 24)
+         | ((memory[offset + 1] & 0xFF) << 16)
+         | ((memory[offset + 2] & 0xFF) << 8)
+         |  (memory[offset + 3] & 0xFF);
+		 
     }
 
     public static int writeShort(byte[] memory, int offset, short value) {
         // TODO: Écrire les 2 octets de 'value'.
+		memory[offset]     = (byte) (value >>> 8);
+        memory[offset + 1] = (byte) value;
         return 2;
     }
 
     public static short readShort(byte[] memory, int offset) {
         // TODO: Lire le short sur 2 octets.
-        return 0;
+        return (short) (((memory[offset] & 0xFF) << 8)
+                      |  (memory[offset + 1] & 0xFF));
     }
 }
