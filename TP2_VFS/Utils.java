c36403e8dcad0c83dfe61f944e544adc936952ae
axelgarnier-del <axel.garnier@iut-rodez.fr>
2026-09-23 23:38:52 +0200

    TP2 mercredi soir

--- TP2_VFS/Utils.java
+++ TP2_VFS/Utils.java
@@ -3,11 +3,10 @@ public class Utils {
     public static int writeInt(byte[] memory, int offset, int value) {
         // TODO: Écrire les 4 octets de 'value' dans 'memory'
         // à partir de 'offset', en big-endian.
-		memory[offset]     = (byte) (value >>> 24);
+		memory[offset] = (byte) (value >>> 24);
         memory[offset + 1] = (byte) (value >>> 16);
         memory[offset + 2] = (byte) (value >>> 8);
         memory[offset + 3] = (byte) value;
-		
         return 4;
     }
 
@@ -22,14 +21,13 @@ public class Utils {
 
     public static int writeShort(byte[] memory, int offset, short value) {
         // TODO: Écrire les 2 octets de 'value'.
-		memory[offset]     = (byte) (value >>> 8);
+		memory[offset] = (byte) (value >>> 8);
         memory[offset + 1] = (byte) value;
         return 2;
     }
 
     public static short readShort(byte[] memory, int offset) {
         // TODO: Lire le short sur 2 octets.
-        return (short) (((memory[offset] & 0xFF) << 8)
-                      |  (memory[offset + 1] & 0xFF));
+        return (short) (((memory[offset] & 0xFF) << 8) | (memory[offset + 1] & 0xFF));
     }
 }
